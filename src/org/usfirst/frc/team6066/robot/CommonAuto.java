47d43dbcf5d8541c6302a51c38232c001310bbe5
Gabe Warfield <gbwarfi@gmail.com>
2018-01-15 16:00:54 -0500

    Created classes for common methods

---
+++ src/org/usfirst/frc/team6066/robot/CommonAuto.java
@@ -0,0 +1,16 @@
+package org.usfirst.frc.team6066.robot;
+
+public class CommonAuto {
+	 
+	public static void crossLine() { 
+		
+	}
+	
+	public static void trackScale() { //Code using vision to track scale LEDs and drive the robot there
+		
+	}
+	
+	public static void positionScale() { //Code to manually position the robot after vision
+		
+	}
+}
---
+++ src/org/usfirst/frc/team6066/robot/CommonDrive.java
@@ -0,0 +1,6 @@
+package org.usfirst.frc.team6066.robot;
+
+public class CommonDrive {
+	
+
+}
