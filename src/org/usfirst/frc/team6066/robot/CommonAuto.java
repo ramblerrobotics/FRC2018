4e25ab6879647bfc82ace3b59f04b51db61d6fec
FrenchToastMafia <mabrown@mcbain.org>
2018-01-16 16:13:25 -0500

    robot drive forward

--- src/org/usfirst/frc/team6066/robot/CommonAuto.java
+++ src/org/usfirst/frc/team6066/robot/CommonAuto.java
@@ -1,15 +1,17 @@
 package org.usfirst.frc.team6066.robot;
 
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Timer;
 
 public class CommonAuto {
+	static Timer t = new Timer();
+	static RobotDrive drive = new RobotDrive(0, 1, 2, 3);
 	 
 	public static void crossLine() { 
-		Timer t = new Timer();
 		t.reset();
 		t.start();
 		if (!t.hasPeriodPassed(5)) {
-			
+			drive.tankDrive(.2,.2);
 		} else {
 			t.stop();
 		}
