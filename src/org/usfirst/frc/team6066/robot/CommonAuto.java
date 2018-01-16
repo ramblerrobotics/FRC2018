4a4f636aea0704d4ddfd3756fc903b5a74ab506f
ArieGen <ariegentry@gmail.com>
2018-01-16 16:04:27 -0500

    added timer to run for 5 seconds

--- src/org/usfirst/frc/team6066/robot/CommonAuto.java
+++ src/org/usfirst/frc/team6066/robot/CommonAuto.java
@@ -1,9 +1,18 @@
 package org.usfirst.frc.team6066.robot;
 
+import edu.wpi.first.wpilibj.Timer;
+
 public class CommonAuto {
 	 
 	public static void crossLine() { 
-		
+		Timer t = new Timer();
+		t.reset();
+		t.start();
+		if (!t.hasPeriodPassed(5)) {
+			
+		} else {
+			t.stop();
+		}
 	}
 	
 	public static void trackScale() { //Code using vision to track scale LEDs and drive the robot there
