b307bd9fe3260b6e8480a8e87e275ec656d3f9a2
Gabe Warfield <gbwarfi@gmail.com>
2018-01-29 15:10:33 -0500

    started auto stuff

--- src/org/usfirst/frc/team6066/robot/CommonAuto.java
+++ src/org/usfirst/frc/team6066/robot/CommonAuto.java
@@ -4,24 +4,15 @@ import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Timer;
 
 public class CommonAuto {
-	static Timer t = new Timer();
-	static RobotDrive drive = new RobotDrive(0, 1, 2, 3);
-	 
-	public static void crossLine() { 
+
+	DriveTrain drive = new DriveTrain();
+	Timer t = new Timer();
+	
+	public void driveForward(double speed, int time) { //Drives forward for time at speed
 		t.reset();
 		t.start();
-		if (!t.hasPeriodPassed(5)) {
-			drive.tankDrive(.2,.2);
-		} else {
-			t.stop();
+		if(t.get() < time) {
+			drive.setMotorSpeeds(speed, speed, 0);
 		}
-	}
-	
-	public static void trackScale() { //Code using vision to track scale LEDs and drive the robot there
-		
-	}
-	
-	public static void positionScale() { //Code to manually position the robot after vision
-		
 	}
 }
