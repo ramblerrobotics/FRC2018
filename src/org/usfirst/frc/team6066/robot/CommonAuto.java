b7b5efed5ce909ead85bfd1a1267070e3ec504f8
Gabe Warfield <gbwarfi@gmail.com>
2018-02-20 15:20:27 -0500

    forog t file

--- src/org/usfirst/frc/team6066/robot/CommonAuto.java
+++ src/org/usfirst/frc/team6066/robot/CommonAuto.java
@@ -1,25 +1,37 @@
 package org.usfirst.frc.team6066.robot;
 
-import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.Timer;
+import java.io.*;
 
 public class CommonAuto {
 
-	DriveTrain drive;
-	Timer timer;
+	Reader r;
+	AutoController auto;
+	char[] data;
 	
-	public CommonAuto() {
-		drive = new DriveTrain();
-		timer = new Timer();
-	}
-	
-	public void driveForwardUntil(double speed, double time) {
-		timer.reset();
-		timer.start();
-		while(timer.get() <= time) {
-		drive.tankDrive(1, 1, speed);
+	public CommonAuto() throws IOException {
+		try {
+			r = new FileReader("/home/lvuser/auto.txt");
+			auto = new AutoController();
+		} catch(IOException e) {
+			System.out.println("Failed to open file");
+		}
+		for (int i = 0; i == -1; i++) {
+			data[i] = (char) r.read();
 		}
 	}
+	
+	public int readDrive() throws IOException {
+		int ins = 9;
+		for(int i = 0; i < data.length; i++) {
+			if(data[i] == '0') ins = 0;
+			if(data[i] == '1') ins = 1;
+			if(data[i] == '2') ins = 2;
+			if(data[i] == '3') ins = 3;
+		}
+		r.close();
+		return ins;
+	}
+	
 	
 	
 	
